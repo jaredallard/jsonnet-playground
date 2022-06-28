883fd8c71b8f154b7abf8007cb55e5647dc20d98
Jared Allard <jaredallard@users.noreply.github.com>
2022-06-28 00:32:40 -0700

    fix: SEO, github link that is horrible looking (#4)

--- web/components/main.js
+++ web/components/main.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 import React from 'react'
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
 
@@ -13,13 +14,13 @@ export default function Main() {
     setEditor(e);
 
     // If we have an ID, load the contents in
-    const id = window.location.href.split('#')[1] 
+    const id = window.location.href.split('#')[1]
     if (!id) {
       return
     }
 
     const req = await fetch(`/api/v1/code/${id}`)
-    const res = await req.json() 
+    const res = await req.json()
     e.setValue(res.contents)
     editor = e
     run()
@@ -62,45 +63,57 @@ export default function Main() {
     window.location.href = `/#${res.id}`
   }
 
+
   return (
-    <body className="light tabwidth-4 ok withsidebar">
-      <div className="header">
-        <div className="logo"></div>
-        <div className="menu">
-          <span className="title">Jsonnet Playground</span>
-          <button onClick={run}>Run</button>
-          {
-            // <button onClick={}>Format <cmd>⌘+S</cmd></button>
-          }
-          <button onClick={share}>Share</button>
-        </div>
-      </div>
-      <div className="body-wrapper">
-        <div className="content-wrapper">
-          <div className="editor-wrapper">
-            <Editor
-              height="90vh"
-              defaultLanguage="text"
-              defaultValue="{}"
-              onMount={handleEditorDidMount}
-            />
-            <div className="shadow"><ol></ol></div>
+    <div>
+      <Head>
+        <title>Jsonnet Playground | jsonnet.dev</title>
+        <meta name="title" content="Jsonnet Playground" />
+        <meta name="description" content="Jsonnet playground with running, saving, and more" />
+        <meta name="keywords" content="jsonnet,playground,editor,online" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+      </Head>
+      <div className="light tabwidth-4 ok withsidebar">
+        <div className="header">
+          <div className="logo"></div>
+          <div className="menu">
+            <span className="title">Jsonnet Playground</span>
+            <button onClick={run}>Run</button>
+            <button onClick={share}>Share</button>
           </div>
-          <div className="output-wrapper">
-            <div className="help">
-              <pre>{output}</pre>
+          <div className="settings">
+            <a href="https://github.com/rgst-io/jsonnet-playground" className="button">Github</a>
+          </div>
+        </div>
+        <div className="body-wrapper">
+          <div className="content-wrapper">
+            <div className="editor-wrapper">
+              <Editor
+                height="90vh"
+                defaultLanguage="text"
+                defaultValue="{}"
+                onMount={handleEditorDidMount}
+              />
+              <div className="shadow"><ol></ol></div>
+            </div>
+            <div className="output-wrapper">
+              <div className="help">
+                <pre>{output}</pre>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="splitter col"></div>
-      <div className="log-wrapper">
-        <div className="log">
-          <div></div>
-          <div className={`status ${error ? 'error': ''}`}>{error}</div>
-          <div className="splitter row"></div>
+        <div className="splitter col"></div>
+        <div className="log-wrapper">
+          <div className="log">
+            <div></div>
+            <div className={`status ${error ? 'error' : ''}`}>{error}</div>
+            <div className="splitter row"></div>
+          </div>
         </div>
       </div>
-    </body>
+    </div>
   )
 }
